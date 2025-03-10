7028837578b786eecf154ac775d0132b8f7fc868
j4munoff <jamunoz412@gmail.com>
2025-03-10 23:21:15 +0100

    Actualizado a JamunozDev

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Preparación OSCP",
-        "excerpt":"Preparación para el OSCP (by s4vitar) Penetration Testing with Kali Linux (PWK) course and Offensive Security Certified Professional (OSCP) Cheat Sheet Esta guía es la misma que tengo publicada en mi Gist: https://gist.github.com/s4vitar/b88fefd5d9fbbdcc5f30729f7e06826e Índice y Estructura Principal Preparación para el OSCP (by s4vitar) - Penetration Testing with Kali Linux (PWK)...","categories": ["Certificaciones"],
+        "excerpt":"# Preparación para el OSCP (by s4vitar) ![OSCP Image](http://funkyimg.com/i/2MPB4.png) #### Penetration Testing with Kali Linux (PWK) course and Offensive Security Certified Professional (OSCP) Cheat Sheet Esta guía es la misma que tengo publicada en mi Gist: * [https://gist.github.com/s4vitar/b88fefd5d9fbbdcc5f30729f7e06826e](https://gist.github.com/s4vitar/b88fefd5d9fbbdcc5f30729f7e06826e) ## Índice y Estructura Principal - [Preparación para el OSCP (by s4vitar)](#preparación-para-el-oscp-by-s4vitar)...","categories": ["Certificaciones"],
         "tags": ["OSCP","Offensive Security","Pentesting","Examen","Guías"],
         "url": "http://localhost:4000/oscp-preparacion/",
         "teaser":"http://localhost:4000/assets/images/oscp-preparation/oscp-logo.png"}]
